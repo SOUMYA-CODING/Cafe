903ec20e9d6ea5e1e0fafdccbd5f15849d752d60
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-07-18 18:45:08 +0530

    confirm

--- cafe/cafe/settings.py
+++ cafe/cafe/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'cafe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cafe_db',
+        'USER': 'root',
+        'PASSWORD': '1992',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
