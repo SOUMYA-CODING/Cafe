0c5b0d14167a00ebd2197e57b4e570507826208b
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-07-22 15:44:51 +0530

    confirm

--- CafeProject/FoodApp/admin.py
+++ CafeProject/FoodApp/admin.py
@@ -4,6 +4,8 @@ from . models import Food
 
 class FoodAdim(admin.ModelAdmin):
     list_display = ('f_category', 'f_name', 'f_price', 'f_is_available')
-
+    list_editable = ('f_is_available',)
+    search_fields = ('f_name',)
+    list_filter = ('f_is_available',)
 
 admin.site.register(Food, FoodAdim)
