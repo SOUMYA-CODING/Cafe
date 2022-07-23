9dd0a9b7e799b06cac75cd0dcb316f22d5509f98
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-07-23 23:28:04 +0530

    confirm

--- CafeProject/CafeProject/urls.py
+++ CafeProject/CafeProject/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('MyApps.urls')),
+    path('food/', include('FoodApp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
