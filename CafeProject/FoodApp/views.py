a582480979b67d8c70195211eaac538b6edcb8ee
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-07-23 23:28:10 +0530

    confirm

--- CafeProject/FoodApp/views.py
+++ CafeProject/FoodApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . models import Food
 
-# Create your views here.
+# Menu Page
+def MenuPage(request):
+    foodmenu = Food.objects.all()
+    context = {'foodmenu': foodmenu}
+    return render(request, 'food/menu.html', context)
