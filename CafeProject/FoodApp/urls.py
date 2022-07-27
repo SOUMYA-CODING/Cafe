47a56a1eb93f65da2ecc45a19fa18236dda29358
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-07-27 18:28:40 +0530

    confirm

--- CafeProject/FoodApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('menu/', views.MenuPage, name="menu_page"),
-    path('details/<int:id>/', views.DetalisPage, name="details_page"),
-]
