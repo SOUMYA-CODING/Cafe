4f35f9f6c3d3f75f32d4cb081056eca2fac3998f
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-07-27 18:29:20 +0530

    confirm

--- CafeProject/MyApps/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . models import FoodCategory
-
-
-class FoodCategoryAdmin(admin.ModelAdmin):
-    list_display = ('fc_name', 'fc_craeted_at')
-
-
-admin.site.register(FoodCategory, FoodCategoryAdmin)
