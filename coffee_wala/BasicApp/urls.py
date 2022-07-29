4f4335c0b40d3ac0c2be66254ce720d75702f043
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-07-29 10:21:59 +0530

    confirm

--- coffee_wala/BasicApp/urls.py
+++ coffee_wala/BasicApp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.Index, name="index"),
     path('about/', views.About, name="about"),
     path('contact', views.Contact, name="contact"),
+    path('search', views.Search, name="search"),
+    path('searchbar', views.SearchBtn, name="searchbar"),
 ]
--- coffee_wala/BasicApp/views.py
+++ coffee_wala/BasicApp/views.py
@@ -19,3 +19,19 @@ def About(request):
 # Contact Page
 def Contact(request):
     return render(request, 'basicpages/contact.html')
+
+
+# Search Page
+def Search(request):
+    return render(request, 'basicpages/search.html')
+
+
+def SearchBtn(request):
+    if request.method == "POST":
+        food_name = request.POST.get("searchinput")
+        foodItems = FoodItem.objects.filter(food_name=FoodItem.name)
+
+        context = {
+            'foodItems': foodItems
+        }
+    return render(request, 'basicpages/search.html', context)
