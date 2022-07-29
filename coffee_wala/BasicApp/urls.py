7f3a4e5776c2161dda421e5aafbf391cfadbf4c2
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-07-30 01:12:06 +0530

    confirm

--- coffee_wala/BasicApp/urls.py
+++ coffee_wala/BasicApp/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('about/', views.About, name="about"),
     path('contact', views.Contact, name="contact"),
     path('search', views.Search, name="search"),
-    path('searchbar', views.SearchBtn, name="searchbar"),
 ]
--- coffee_wala/BasicApp/views.py
+++ coffee_wala/BasicApp/views.py
@@ -1,7 +1,6 @@
 from unicodedata import name
 from django.shortcuts import render
 from FoodApp.models import FoodItem
-from django.db.models import Q
 
 
 # Index Page
@@ -23,16 +22,6 @@ def Contact(request):
     return render(request, 'basicpages/contact.html')
 
 
-# Search Page
+# Search
 def Search(request):
-    return render(request, 'basicpages/search.html')
-
-
-def SearchBtn(request):
-    if request.method == "GET":
-        food_name = request.GET.get("searchinput")
-        foods = FoodItem.objects.filter(Q(name__icontains=food_name))
-        context = {
-            'food': foods,
-        }
-    return render(request, 'basicpages/search.html', context)
+    return render(request, 'food/menu.html')
